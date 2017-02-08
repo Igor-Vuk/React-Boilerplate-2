f403ee333ccf95dbeee6bcd64a445c9f718ce74a
Igor Vukelić <igvukelic@gmail.com>
2017-02-08 02:14:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# React Boilerplate 2 #
+## Boilerplate to start your React project ##
+- - - -
+### Included ###
+* webpack
+* jQuery
+* Foundation framework
+* SCSS
+* source maps
+* browser history
+
+### Global dependencies ###
+* webpack
+
+### Requirements ###
+* Node.js
+* GitBash
---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+// Create our app
+var app = express();
+const PATH = require("path");
+const PORT = process.env.PORT || 3000;
+
+app.use(function (req, res, next) {
+    if(req.headers["x-forwarded-proto"] === "https") {
+        res.redirect("http://" + req.hostname + req.url);
+    } else {
+        next();
+    }
+});
+
+
+app.use(express.static("public"));
+
+//to use browser history
+app.get("*", function(req, res) {
+    res.sendFile(PATH.resolve(__dirname, "public", "index.html"))
+})
+
+
+
+
+app.listen(PORT, function () {
+    console.log("Express server is up on port " + PORT);
+});
